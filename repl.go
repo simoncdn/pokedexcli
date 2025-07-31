132c22cd28b206dfe4ef6af477f2a975d762cd39
Simon Cardona <cdna.simon@gmail.com>
2025-07-31 14:59:36 +0200

    feat: add repl

--- repl.go
+++ repl.go
@@ -1,8 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"bufio"
+	"os"
 	"strings"
 )
+
+func startRepl(){
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		userInput := scanner.Text()
+		userCommand := cleanInput(userInput)[0]
+
+		fmt.Printf("Your command was: %s\n", userCommand)
+	}
+}
 
 func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
